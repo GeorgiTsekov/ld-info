b7d8bf871b819d7f26c83d45b96a2ec6796f60d5
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-11-20 00:58:49 +0200

    LDInfoDbContext added

---
+++ LDInfo/LDInfo.Data/LDInfoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LDInfo.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LDInfo.Data
+{
+    public class LDInfoDbContext : DbContext
+    {
+        public LDInfoDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TimeLog> TimeLogs { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
