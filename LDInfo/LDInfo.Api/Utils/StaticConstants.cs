da2a79230c7969d72c6004a7febb1e383f3bc165
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-11-20 20:34:39 +0200

    add few staticconstants

--- LDInfo/LDInfo.Api/Utils/StaticConstants.cs
+++ LDInfo/LDInfo.Api/Utils/StaticConstants.cs
@@ -3,5 +3,16 @@
     public static class StaticConstants
     {
         public const string CONNECTION_STRING = "LDInfoConnectionString";
+        public static string[] FIRST_NAMES = { "John", "Gringo", "Mark", "Lisa", "Maria", "Sonya", "Philip", "Jose", "Lorenzo", "George", "Justin" };
+        public static string[] LAST_NAMES = { "Johnson", "Lamas", "Jackson", "Brown", "Mason", "Rodriguez", "Roberts", "Thomas", "Rose", "McDonalds" };
+        public static string[] DOMAINS = { "hotmail.com", "gmail.com", "live.com" };
+        public static string[] PROJECT_NAMES = { "My own", "Free Time", "Work" };
+        public const float MAX_HOURS = 8.00F;
+        public const float MIN_HOURS = 0.25F;
+        public const int MIN_DATE = 0;
+        public const int MAX_DATE = 10;
+        public const int USERS_COUNT = 100;
+        public const int TIME_LOGS_MIN_COUNT = 1;
+        public const int TIME_LOGS_MAX_COUNT = 20;
     }
 }
