5e7024bf29dc0646f366ae30c21c2f32c1de8238
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-11-20 14:31:19 +0200

    UsersController created

---
+++ LDInfo/LDInfo.Api/Features/Users/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using LDInfo.Api.Features.Users.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LDInfo.Api.Features.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+        private readonly IMapper mapper;
+
+        public UsersController(IUserService userService, IMapper mapper)
+        {
+            this.userService = userService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var models = await this.userService.AllAsync();
+
+            var modelsDtos = mapper.Map<List<UserDto>>(models);
+
+            return Ok(modelsDtos);
+        }
+    }
+}
