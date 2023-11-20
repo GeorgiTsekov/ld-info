b3f61aaccd4872ebda7942f8a5ecce3e8034a27d
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-11-20 14:29:05 +0200

    AutomapperProfiles added

---
+++ LDInfo/LDInfo.Api/Features/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using LDInfo.Api.Features.Projects.Model;
+using LDInfo.Api.Features.TimeLogs.Models;
+using LDInfo.Api.Features.Users.Models;
+using LDInfo.Data.Entities;
+
+namespace LDInfo.Api.Features
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<UserDto, User>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<Project, ProjectDto>().ReverseMap();
+            CreateMap<ProjectDto, Project>().ReverseMap();
+            CreateMap<TimeLogDto, TimeLog>().ReverseMap();
+            CreateMap<TimeLog, TimeLogDto>().ReverseMap();
+        }
+    }
+}
