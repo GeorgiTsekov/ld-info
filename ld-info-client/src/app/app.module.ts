642878270d0faf8b4a5934084c57822d929bb3d6
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-11-24 16:15:43 +0200

    new components added to the app.module

--- ld-info-client/src/app/app.module.ts
+++ ld-info-client/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { TimelogsComponent } from './timelogs/timelogs.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    TimelogsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
